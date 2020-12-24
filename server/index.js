2111438b36f45da6280d4e73b5855b0d49e0c4ae
George Emmanuel <rustydcoder@gmail.com>
2020-12-24 08:02:59 +0100

    set up server

---
+++ server/index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// set up mongoose
+mongoose.connect(
+  process.env.MONGODB_URL,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err) => {
+    if (err) throw err;
+    console.log("Mongodb connection established");
+  }
+);
+
+// middlewares
+app.use(bodyParser.json());
+app.use(cors());
+
+// set routes
+const userRoute = require("./routes/userRouter");
+
+app.use("/users", userRoute);
+
+// startup server
+app.listen(PORT, () => console.log(`server running on ${PORT}`));
